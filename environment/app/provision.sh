fbdb9c96d29af6f2658401f4742e9879958621b3
Elliot Harris <elliotharris@tcadmins-MacBook-Air.local>
2020-04-15 12:41:47 +0100

    developer enviroment mad with passing tests

--- environment/app/provision.sh
+++ environment/app/provision.sh
@@ -8,8 +8,8 @@ sudo apt-get install python3.7 -y
 
 sudo apt-get install python3-pip -y
 
-sudo pip3 install -r /home/ubuntu/job_watch_project/requirements.txt
+sudo pip3 install -r /home/ubuntu/app/requirements.txt
 
 sudo mkdir Downloads
 
-sudo chmod 777 ~/Downloads/
+sudo chmod 777 /home/vagrant/Downloads/
